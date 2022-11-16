2d38ba2ca0385a32e6518720c5dfe2beb7b8be27
john <kiokoj81@gmail.com>
2022-11-16 16:49:40 +0300

    Made changes recommended by Reviewer

--- src/redux/categories/categories.js
+++ src/redux/categories/categories.js
@@ -3,18 +3,16 @@ const CHECK_STATUS = 'CHECK_STATUS';
 const checkStatusReducer = (state = [], action) => {
   switch (action.type) {
     case CHECK_STATUS:
-      return state.filter((category) => category.id === action.id);
+      return action.payload;
 
     default:
       return state;
   }
 };
 
-const nextCategoryId = 0;
-
 export const checkStatus = () => ({
   type: CHECK_STATUS,
-  id: nextCategoryId + 1,
+  payload: 'Check Status',
 });
 
 export default checkStatusReducer;
