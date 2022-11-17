6a61f811c99d4e25c2600d7481068dc50bfe6d07
john <kiokoj81@gmail.com>
2022-11-17 22:17:30 +0300

    Linking react with redux

--- src/components/AddBooks.js
+++ src/components/AddBooks.js
@@ -1,13 +1,40 @@
-import React from 'react';
+import { React, useState } from 'react';
+import { PropTypes } from 'prop-types';
 
-const AddBooks = () => (
-  <div className="wrapper">
-    <form>
-      <input type="text" placeholder="Book title" />
-      <input type="text" placeholder="author" />
-      <button type="submit">Add Book</button>
-    </form>
-  </div>
-);
+const AddBook = ({ AddNewBook }) => {
+  const [NewBook, setNewBook] = useState({ title: '', author: '' });
 
-export default AddBooks;
+  return (
+    <div id="form-container">
+      <h2>ADD NEW BOOK</h2>
+      <form onSubmit={(e) => {
+        AddNewBook(e, NewBook);
+        setNewBook({ title: '', author: '' });
+      }}
+      >
+        <input
+          type="text"
+          name="title"
+          placeholder="Book Ttitle"
+          value={NewBook.title}
+          required
+          onChange={(e) => setNewBook({ ...NewBook, title: e.target.value })}
+        />
+        <input
+          type="text"
+          name="author"
+          placeholder="Book Author"
+          value={NewBook.author}
+          required
+          onChange={(e) => setNewBook({ ...NewBook, author: e.target.value })}
+        />
+        <button type="submit" id="add-book">ADD BOOK</button>
+      </form>
+    </div>
+  );
+};
+AddBook.propTypes = {
+  AddNewBook: PropTypes.func.isRequired,
+};
+
+export default AddBook;
